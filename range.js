4f81ebd359cd927198f608f63d4d05dbd373e9e9
Patricio Garcia <patgarcia@users.noreply.github.com>
2021-04-07 15:02:57 -0400

    Created python's range logic in JS

---
+++ range.js
@@ -0,0 +1,8 @@
+// Works as python's range
+
+const range = (n, start = 0, step = 1) =>
+  Object.keys(Array(n).fill())
+    .map((x) => Number(x))
+    .filter((x) => x + start < n)
+    .map((x) => x + start)
+    .filter((x, i) => !(i % step));
