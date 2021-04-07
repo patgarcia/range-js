5fec017ed00fe3d51cb5cbfd8f379687a83519c2
Patricio Garcia <patgarcia@users.noreply.github.com>
2021-04-07 15:21:11 -0400

    Fix prettier format error

--- range.js
+++ range.js
@@ -1,8 +1,9 @@
 // Works as python's range
 
-const range = (n, start = 0, step = 1) =>
-  Object.keys(Array(n).fill())
+const range = (n, start = 0, step = 1) => {
+    return Object.keys(Array(n).fill())
     .map((x) => Number(x))
     .filter((x) => x + start < n)
     .map((x) => x + start)
     .filter((x, i) => !(i % step));
+}
